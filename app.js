e9b0079a909d6fdf6c98d17a576fe7735fdc5959
julianhuertas91 <92815555+julianhuertas91@users.noreply.github.com>
2022-03-28 15:26:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const port = 3030;
+app.use(express.static('./public'));
+app.listen(port, () => {
+  console.log(`Está corriendo el servidor en el puerto: ${port}`)
+})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'./views/index.html'))
+})
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/signin.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
